ca269cd386ffa6017da2db4f7e6a03371a855fd0
DevGuideDaily <devguidedaily@gmail.com>
2020-10-11 12:52:44 +0200

    Tidy up finger print logic

--- lib/finger-print.ts
+++ lib/finger-print.ts
@@ -2,6 +2,13 @@ import { FileData, FileSystem } from "./types";
 import { createHash } from "crypto";
 import { parse as parsePath, join as joinPath, relative as getRelativePath } from "path";
 
+const defaultSkip = [
+	/\.html/,
+	/\.pug/
+];
+
+const hashLength = 8;
+
 interface FingerPrintFileParams {
 	fileData: FileData;
 	absSrcDirPath: string;
@@ -9,6 +16,7 @@ interface FingerPrintFileParams {
 	outExt: string;
 	absOutDirPath: string;
 	fileSystem: FileSystem;
+	skip?: RegExp[];
 }
 
 export const fingerPrintFile = ({
@@ -17,18 +25,24 @@ export const fingerPrintFile = ({
 	absSrcFilePath,
 	outExt,
 	absOutDirPath,
-	fileSystem
+	fileSystem,
+	skip = defaultSkip
 }: FingerPrintFileParams) => {
-	const hash = hashFileData(fileData);
+	const hash = getFileHash(absSrcFilePath, skip, fileData);
 	const relativePath = getRelativePath(absSrcDirPath, absSrcFilePath);
 	const { dir: relativeDir } = parsePath(relativePath);
 	const { name } = parsePath(absSrcFilePath);
-	const absOutPath = joinPath(absOutDirPath, relativeDir, `${name}.${hash}${outExt}`);
+	const absOutPath = joinPath(absOutDirPath, relativeDir, `${name}${hash}${outExt}`);
 	fileSystem.write(absOutPath, fileData);
 	return absOutPath;
 }
 
-export const hashFileData = (fileData: FileData) => {
+const getFileHash = (absSrcFilePath: string, skip: RegExp[], fileData: FileData) => {
+	const shouldSkip = skip.some(regex => regex.test(absSrcFilePath));
+	return shouldSkip ? "" : `.${hashFileData(fileData).slice(0, hashLength)}`;
+}
+
+const hashFileData = (fileData: FileData) => {
 	const hash = createHash("md5");
 	if (typeof fileData === "string") {
 		return hash.update(fileData).digest("hex");
