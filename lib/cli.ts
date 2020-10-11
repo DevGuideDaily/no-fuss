17a3f2b50b60acb5a7f87ccef6a4124fe3464e83
DevGuideDaily <devguidedaily@gmail.com>
2020-10-11 18:41:09 +0200

    Improve CLI

--- lib/cli.ts
+++ lib/cli.ts
@@ -6,12 +6,23 @@ const program = new commander.Command();
 program.version("0.0.1");
 program.option("-s, --src-dir <path>", "Source directory");
 program.option("-o, --out-dir <path>", "Output directory");
-program.option("-p, --port <number>", "Port number (5000)")
 
-program.command("watch").action(cmd => runWatch(cmd.parent));
-program.command("build").action(cmd => runBuild(cmd.parent));
+program.command("watch")
+	.description("Watch source files for change and build incrementally")
+	.action(cmd => runWatch(cmd.parent));
+
+program.command("build")
+	.description("Build source files once for production")
+	.action(cmd => runBuild(cmd.parent));
+
 program.command("serve", { isDefault: true })
-	.action(cmd => runServe(cmd.parent));
+	.description("Watch source files for change and start a development server")
+	.option("-p, --port <number>", "Port number (5000)")
+	.action(cmd => runServe({
+		srcDir: cmd.parent.srcDir,
+		outDir: cmd.parent.outDir,
+		port: cmd.port
+	}));
 
 console.log();
 program.parse(process.argv);
